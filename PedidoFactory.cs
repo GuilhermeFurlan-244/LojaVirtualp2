9b12ff74db19d127c11397879e56287a5a073c7d
GuilhermeFurlan-244 <guilherme.louzada@sou.unifeb.edu.br>
2025-06-05 12:00:04 -0300

    Adicionado serviço de log com injeção de dependência. O PedidoFactory agora registra cada etapa da criação do pedido, incluindo cliente e itens adicionados. Essa abordagem melhora a rastreabilidade e segue o princípio de Inversão de Dependência (SOLID).

--- PedidoFactory.cs
+++ PedidoFactory.cs
@@ -9,14 +9,17 @@ namespace LojaVirtualP2
     public class PedidoFactory
     {
         private readonly IDescontoStrategy _estrategiaDesconto;
+        private readonly ILogService _log;
 
-        public PedidoFactory(IDescontoStrategy estrategiaDesconto)
+        public PedidoFactory(IDescontoStrategy estrategiaDesconto, ILogService log  )
         {
             _estrategiaDesconto = estrategiaDesconto;
+            _log = log;
         }
 
         public Pedido CriarPedido(int idpedido, Cliente cliente,List<(Produto produto,int quantidade)> itens)
         {
+            _log.Registrar($"Iniciando criação do pedido #{idpedido}para o cliente {cliente.Nome}");
             var pedido = new Pedido(idpedido, cliente);
 
             foreach (var(produto,quantidade)in itens)
@@ -27,7 +30,10 @@ namespace LojaVirtualP2
                     DescontoStrategy = _estrategiaDesconto
                 };
                 pedido.AdicionarItem(itemPedido);
+                _log.Registrar($"Pedido #{quantidade}x '{produto.Nome}' ao pedido # {idpedido}");
             }
+
+            _log.Registrar($"Pedido #{idpedido} criado com sucesso com valor total de R$ {pedido.ValorTotal:F2}");
             return pedido;
         }
 
--- RelatorioService.cs
+++ RelatorioService.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace LojaVirtualP2
 {
-    class RelatorioService
+    public class RelatorioService
     {
+        private readonly List<Pedido> _pedidos;
+
+        public RelatorioService(List<Pedido> pedidos)
+        {
+            _pedidos = pedidos ?? new List<Pedido>();
+        }
+        public void ExibirRelatorio()
+        {
+            Console.WriteLine("====== RELATÓRIO DE PEDIDOS =====");
+
+            foreach (var pedido in _pedidos)
+            {
+                Console.WriteLine($"\nPedido ID: {pedido.Id}");
+                Console.WriteLine($"Cliente: {pedido.Cliente.Nome} | CPF: {pedido.Cliente.CPF}");
+                Console.WriteLine($"Data: {pedido.Data}");
+                Console.WriteLine("Itens");
+
+                foreach (var item in pedido.Itens)
+                {
+                    Console.WriteLine($"- {item.Produto.Nome} | Categoria: {item.Produto.Categoria}| Qtd: {item.Quantidade} | Preço: R${item.Produto.Preco} | Total c/ desconto: R${item.GetValorTotal()}); ");
+                }
+                Console.WriteLine($"Valor Total do Pedido: R${pedido.ValorTotal}");
+                Console.WriteLine("=======================");
+            }
+        }
     }
 }
